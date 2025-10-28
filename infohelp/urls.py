3f5606a71fcff870fd1624aa9bb28e8c534b7c7e
Antônio Antunes <antunesantonio91@gmail.com>
2025-10-28 09:52:18 -0300

    feat: adiciona templates e suas urls e views

--- infohelp/urls.py
+++ infohelp/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('inicio/', views.inicio, name="inicio"),
+    path('cursos/', views.cursos, name="cursos"),
+    path('biblioteca/', views.biblioteca, name="biblioteca"),
+    path('login/', views.login, name="login"),
+    path('cadastro/', views.cadastro, name="cadastro"),
 ]
--- infohelp/views.py
+++ infohelp/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+
+def inicio(request):
+    return render(request, "inicio.html")
+
+def cursos(request):
+    return render(request, "cursos.html")
+
+def biblioteca(request):
+    return render(request, "biblioteca.html")
+
+def login(request):
+    return render(request, "login.html")
+
+def cadastro(request):
+    return render(request, "cadastro.html")
